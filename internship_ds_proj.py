aff06f8335059999e4d36aa08af7dc34e69c5f9a
ShreyTayal <36303919+ShreyTayal@users.noreply.github.com>
2023-01-18 15:49:17 -0500

    Add files via upload

---
+++ internship_ds_proj.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+
+df = pd.read_csv("user_data.csv")
+
+#returns cleaned format of the dataframe, taking out any columns with no data
+def clean_data(df):
+    df = df.dropna(axis=1, how='all')
+    df = df.fillna(0)
+    return df
+
+#compares the user rating to the averages
+def compare_user(input):
+    return
+
+user_df = clean_data(df)
+print(user_df)
