f44c407f27ec35ddc56bfbc4ed4397cd7bfc078a
ShreyTayal <36303919+ShreyTayal@users.noreply.github.com>
2023-01-18 16:06:27 -0500

    Update internship_ds_proj.py

--- internship_ds_proj.py
+++ internship_ds_proj.py
@@ -7,7 +7,7 @@ df = pd.read_csv("user_data.csv")
 #returns cleaned format of the dataframe, taking out any columns with no data
 def clean_data(df):
     df = df.dropna(axis=1, how='all')
-    df = df.fillna(0)
+    '''df = df.fillna(0)'''
     return df
 
 user_df = clean_data(df)
