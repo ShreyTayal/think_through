43f2cf377b91423d97a426e51f318d7de8575905
akshatchavann <121521773+akshatchavann@users.noreply.github.com>
2023-01-20 23:20:49 -0500

    Update internship_ds_proj.py

--- internship_ds_proj.py
+++ internship_ds_proj.py
@@ -11,4 +11,16 @@ def clean_data(df):
     return df
 
 user_df = clean_data(df)
+
+#make the phone numbers the index collumn and remove duplicate IDs
+user_df = user_df.set_index("UserID (Phone Number)")
+user_df = user_df.drop_duplicates()
+
+#delete old average row and add an dynamic average row at the end of all the numbers
+user_df = user_df.drop(user_df.index[-1])
+user_df.loc["Avg"] = np.nan
+user_df.loc["Avg"] = user_df.mean(numeric_only=True, axis=0)
+
+
+
 print(user_df)
