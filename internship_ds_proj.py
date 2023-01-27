7932682fedb62e6f33f37b0edbb6375831dc442b
ShreyTayal <36303919+ShreyTayal@users.noreply.github.com>
2023-01-27 16:52:11 -0500

    Update internship_ds_proj.py

--- internship_ds_proj.py
+++ internship_ds_proj.py
@@ -2,25 +2,29 @@ import numpy as np
 import pandas as pd
 
 
-df = pd.read_csv("user_data.csv")
+df = pd.read_csv('user_data.csv')
 
 #returns cleaned format of the dataframe, taking out any columns with no data
+
 def clean_data(df):
     df = df.dropna(axis=1, how='all')
-    '''df = df.fillna(0)'''
+    
+    #make the phone numbers the index collumn and remove duplicate IDs
+    df = df.set_index("UserID (Phone Number)")
+    df.index.drop_duplicates()
+    
+    #delete old average row and add an dynamic average row at the end of all the numbers
+    df = df.drop(df.index[-1])
+    df.loc["Avg"] = np.nan
+    df.loc["Avg"] = df.mean(numeric_only=True, axis=0)
     return df
 
 user_df = clean_data(df)
 
-#make the phone numbers the index collumn and remove duplicate IDs
-user_df = user_df.set_index("UserID (Phone Number)")
-user_df = user_df.drop_duplicates()
+#testing the creation of the y-coordinate of the political quadrant using two of the questions
+def create_y(df):
+    y = df.loc[:, ['Police Brutality', 'Free Market']]
+    y["Averages"] = [np.mean(a=y.loc[item, :]) for item in y.index]
+    return y
 
-#delete old average row and add an dynamic average row at the end of all the numbers
-user_df = user_df.drop(user_df.index[-1])
-user_df.loc["Avg"] = np.nan
-user_df.loc["Avg"] = user_df.mean(numeric_only=True, axis=0)
-
-
-
-print(user_df)
+print(create_y(user_df))
